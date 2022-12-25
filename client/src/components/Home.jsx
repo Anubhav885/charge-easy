d70b5c1dd203ff7249c373a660c577317addda87
Ayush <ayushpersonal2310@gmail.com>
2022-12-25 17:46:25 +0530

    Created Home Section

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import { Box, Button, styled, Typography } from '@mui/material'
+
+import Header from "./Header";
+import HomeSection from "./HomeSection";
+
+const MainContainer = styled(Box)`
+    display : flex;
+    justify-content : space-around;
+    background :#6E6E6E; 
+    height :100vh;
+    flex-direction : column;
+`
+
+const Home = () => {
+    return (
+        <MainContainer>
+            <Header />
+            <HomeSection/>
+        </MainContainer>
+    )
+}
+
+export default Home
